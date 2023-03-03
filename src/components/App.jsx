1630392b94b26d4af59a16b7fad588e33c5be12d
VozzRoman <looper0500@ukr.net>
2023-03-03 14:40:52 +0200

    adding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
+import { ContactList } from './ContcatList/ContactList';
+import { Filter } from './Filter/Filter';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Background } from './Container/Background';
+import { Box } from './Container/Box';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations/operations';
+
+import Loader from './Loader/Loader';
+import { selectError, selectIsLoading } from 'redux/selectors/selectors';
+
 export const App = () => {
+
+	const dispatch = useDispatch();
+	const isLoading = useSelector(selectIsLoading);
+	const error = useSelector(selectError);
+
+	useEffect(()=> {
+		dispatch(fetchContacts());
+	}, [dispatch]);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Background>
+      <Box
+        m="0 auto"
+        maxWidth="500px"
+        background="orange"
+        borderRadius="12px"
+        p="20px"
+        boxShadow="3px 4px 6px #888888"
+      >
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <> 
+          <Filter />
+			 {isLoading && <Loader/>}
+			 {error && <p>{error}</p>}
+          <ContactList />
+        </>
+      </Box>
+    </Background>
   );
 };
---
+++ src/components/Container/Background.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import img from '../../img/back.jpg';
+
+export const Background = styled.div`
+  position: relative;
+  padding: 40px;
+  &::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    background: url(${img});
+    background-size: contain;
+    opacity: 0.2;
+    z-index: -1;
+  }
+`;
